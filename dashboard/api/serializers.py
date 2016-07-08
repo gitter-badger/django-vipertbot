710d8909116234ee6b4f909e1151cc7bbb54c419
Poppabear <servnx@gmail.com>
2016-07-08 16:32:51 -0400

    Preparing dashboard API

---
+++ dashboard/api/serializers.py
@@ -0,0 +1,69 @@
+# from rest_framework.serializers import (
+#     ModelSerializer,
+#     HyperlinkedIdentityField
+# )
+#
+# from django.contrib.auth import get_user_model
+# User = get_user_model()
+#
+# class UserListSerializer(ModelSerializer):
+#     url = HyperlinkedIdentityField(
+#         view_name='users-detail',
+#         lookup_field='id'
+#     )
+#
+#     class Meta:
+#         model = User
+#         fields = [
+#             'id',
+#             'username',
+#             'email',
+#             'first_name',
+#             'last_name',
+#             'url'
+#         ]
+#
+# class UserDetailSerializer(ModelSerializer):
+#     class Meta:
+#         model = User
+#         fields = [
+#             'id',
+#             'username',
+#             'email',
+#             'first_name',
+#             'last_name',
+#         ]
+#
+# class UserCreateSerializer(ModelSerializer):
+#     # todo: serialize validated_data for password ...
+#     class Meta:
+#         model = User
+#         fields = [
+#             'username',
+#             'email',
+#             'password',
+#         ]
+#
+#         extra_kwargs = {
+#             'password': {
+#                 'write_only': True
+#             }
+#         }
+#
+# class UserUpdateSerializer(ModelSerializer):
+#     # todo: add staff/admin fields, serialize validated_data for password ...
+#     class Meta:
+#         model = User
+#         fields = [
+#             'username',
+#             'email',
+#             'password',
+#             'first_name',
+#             'last_name',
+#         ]
+#
+#         extra_kwargs = {
+#             'password': {
+#                 'write_only': True
+#             }
+#         }
--- dashboard/api/views.py
+++ dashboard/api/views.py
@@ -1,3 +1,74 @@
-from django.shortcuts import render
-
-# Create your views here.
+# from django.db.models import Q
+# from rest_framework.filters import (
+#     SearchFilter,
+#     OrderingFilter
+# )
+#
+# from django.contrib.auth.models import User
+#
+# from rest_framework.permissions import (
+#     IsAuthenticated,
+#     IsAdminUser,
+# )
+#
+# from rest_framework.generics import (
+#     ListAPIView,
+#     RetrieveAPIView,
+#     RetrieveUpdateAPIView,
+#     CreateAPIView,
+#     DestroyAPIView,
+# )
+#
+# from .serializers import (
+#     UserCreateSerializer,
+#     UserUpdateSerializer,
+#     UserDetailSerializer,
+#     UserListSerializer,
+# )
+#
+# class UserListApiView(ListAPIView):
+#     serializer_class = UserListSerializer
+#     filter_backends = [SearchFilter, OrderingFilter]
+#     permission_classes = [IsAuthenticated, IsAdminUser]
+#     search_fields = [
+#         'username',
+#         'email',
+#         'first_name',
+#         'last_name',
+#     ]
+#
+#     def get_queryset(self, *args, **kwargs):
+#         queryset_list = User.objects.all()
+#         query = self.request.GET.get('q')
+#         if query:
+#             queryset_list = queryset_list.filter(
+#                 Q(username__icontains=query)|
+#                 Q(email__icontains=query)|
+#                 Q(first_name__icontains=query)|
+#                 Q(last_name__icontains=query)
+#             ).distinct()
+#
+#         return queryset_list
+#
+# class UserDetailApiView(RetrieveAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserDetailSerializer
+#     permission_classes = [IsAuthenticated, IsAdminUser]
+#     lookup_field = 'id'
+#
+# class UserCreateApiView(CreateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserCreateSerializer
+#     permission_classes = [IsAuthenticated, IsAdminUser]
+#
+# class UserUpdateApiView(RetrieveUpdateAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserUpdateSerializer
+#     permission_classes = [IsAuthenticated, IsAdminUser]
+#     lookup_field = 'id'
+#
+# class UserDeleteApiView(DestroyAPIView):
+#     queryset = User.objects.all()
+#     serializer_class = UserDetailSerializer
+#     permission_classes = [IsAuthenticated, IsAdminUser]
+#     lookup_field = 'id'
